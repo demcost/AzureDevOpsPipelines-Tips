9ece43daefa9b4e58b68961c291399cb028d9913
Demetrio Costa <demcost@microsoft.com>
2024-02-17 00:25:22 -0300

    Update main.tf

--- skip-step/main.tf
+++ skip-step/main.tf
@@ -31,13 +31,3 @@ resource "azurerm_kubernetes_cluster" "example-aks1" {
   }
 }
 
-output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.aks-best-practices-rg3.kube_config.0.client_certificate
-  sensitive = true
-}
-
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.aks-best-practices-rg3.kube_config_raw
-
-  sensitive = true
-}
