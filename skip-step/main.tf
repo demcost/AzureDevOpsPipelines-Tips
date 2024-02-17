a992dcf8e1c591e64296b114d482a03499499ec7
Demetrio Costa <demcost@microsoft.com>
2024-02-16 23:10:07 -0300

    Update main.tf

--- skip-step/main.tf
+++ skip-step/main.tf
@@ -9,7 +9,39 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "resourcegroup-test-tbd-3"
-  location = "westeurope"
+resource "azurerm_resource_group" "aks-best-practices-rg3" {
+  name     = "aks-best-practices-rg3"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks-best-practices-rg3" {
+  name                = "example-aks1"
+  location            = azurerm_resource_group.aks-best-practices-rg3.location
+  resource_group_name = azurerm_resource_group.aks-best-practices-rg3.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.aks-best-practices-rg3.kube_config.0.client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks-best-practices-rg3.kube_config_raw
+
+  sensitive = true
 }
